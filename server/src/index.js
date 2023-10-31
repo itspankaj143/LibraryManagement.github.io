3242618846722773b081bb71ab2064731499e726
itspankaj143 <itsmelovezero@gmail.com>
2023-10-31 15:20:41 +0530

    file added

---
+++ server/src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const router = require("./routers/book-routers");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+app.use("/books", router);
+
+mongoose
+  .connect(
+    "mongodb+srv://itsmelovezero:KAiQEOXRss68fcB5@cluster0.boep1ut.mongodb.net/LibraryManagement?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connection Successfull");
+  })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(`Server is ruuning on http://localhost:3000`);
+    });
+  })
+  .catch((e) => {
+    console.error("Error", e);
+  });
